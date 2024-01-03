d2e8c15c5dc9fd73cc1034c45467e117c22fdd0f
noooobdev <nirajbohara10@gmail.com>
2024-01-03 21:50:21 +0545

    style Profile page

--- app/(index)/profile/page.tsx
+++ app/(index)/profile/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Navbar from "@/components/Navbar";
+import Insta from "@/app/_assets/Insta.svg";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
@@ -11,7 +13,7 @@ const page = () => {
           <h1 className="text-3xl">Niraj</h1>
           <div className="flex gap-2">
             <h1>_n1r4j_</h1>
-            <h1 className="text-xs bg-gray-200 p-1 rounded-full text-slate-800">
+            <h1 className="text-[10px] font-light bg-zinc-100 p-2 rounded-full text-zinc-400">
               threads.net
             </h1>
           </div>
@@ -21,6 +23,31 @@ const page = () => {
           <AvatarFallback>MJ</AvatarFallback>
         </Avatar>
       </div>
+      <div className="mt-4 w-full">
+        <h1>This is my Bio</h1>
+
+        <div className="flex items-center mt-6 w-full">
+          <Avatar className="h-5 w-5 border-2 border-zinc-200 absolute">
+            <AvatarImage src="https://www.celebnest.com/uploads/celebs/1/michael_jackson.jpg" />
+            <AvatarFallback>MJ</AvatarFallback>
+          </Avatar>
+          <Avatar className="h-5 w-5 border-2 border-zinc-200 relative left-4">
+            <AvatarImage src="https://www.celebnest.com/uploads/celebs/1/michael_jackson.jpg" />
+            <AvatarFallback>MJ</AvatarFallback>
+          </Avatar>
+          <div className="ml-6 w-full flex items-center justify-between gap-3">
+            <h1 className=" text-sm text-zinc-400 ">8 followers</h1>
+            <Insta />
+          </div>
+        </div>
+      </div>
+
+      <Button
+        variant="outline"
+        className="w-full justify-center m-5 rounded-xl"
+      >
+        Edit Profile
+      </Button>
     </div>
   );
 };
